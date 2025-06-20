73fb1fd19fb644b64af37196c17acf4528ab2fba
EFDillmann <enzodillmann.dev@gmail.com>
2025-06-19 21:44:18 -0300

    (feat)protected routes: -add env config file -add custom hook for onboarding validation and session validation -add MySlot for charge screens

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,32 +1,24 @@
-import { Redirect, Stack } from "expo-router";
-
-import { useAuth } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const { isSignedIn } = useAuth();
-
-  if (isSignedIn) {
-    return <Redirect href="/(protected)/(mainTabs)/home" />;
-  }
-
   return (
-    <Stack screenOptions={{headerShown: false}} >
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="sign-in" />
-      <Stack.Screen 
-        name="sign-in-modal" 
+      <Stack.Screen
+        name="sign-in-modal"
         options={{
           headerShown: true,
 
-          title: "Iniciar sesión", 
+          title: "Iniciar sesión",
           presentation: "modal",
           headerStyle: {
-            backgroundColor: '#2d7a3e',
+            backgroundColor: "#2d7a3e",
           },
-          headerTintColor: '#ffffff',
+          headerTintColor: "#ffffff",
           headerTitleStyle: {
-            color: '#ffffff',
+            color: "#ffffff",
           },
-        }}  
+        }}
       />
       <Stack.Screen name="sign-up" />
     </Stack>
--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,19 +1,11 @@
-import { Redirect, Stack } from "expo-router";
-
-import { useAuth } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
 
 const ProtectedLayout = () => {
-  const { isSignedIn } = useAuth();
-
-  if (!isSignedIn) {
-    return <Redirect href="/(auth)/sign-in" />;
-  }
-
   return (
     <Stack>
       <Stack.Screen name="(mainTabs)" options={{ headerShown: false }} />
       <Stack.Screen name="membership" />
-      <Stack.Screen name="users" />
+      <Stack.Screen name="users/[userId]" />
     </Stack>
   );
 };
