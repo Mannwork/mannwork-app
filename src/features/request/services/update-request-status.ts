5f026767140f790b52186a16989e9c1acdfcc10b
EFDillmann <enzodillmann.dev@gmail.com>
2025-08-06 21:08:45 -0300

    (fix) add update chat status when finalize a request

--- src/features/request/services/update-request-status.ts
+++ src/features/request/services/update-request-status.ts
@@ -2,8 +2,23 @@ import { supabase } from "@/common/lib/supabase/supabaseClient";
 
 export const updateRequestStatus = async (status: string, requestId: string) => {
     const { data, error } = await supabase.from("requests").update({ status }).eq("id", requestId);
+    const {data: data2, error: error2} = await supabase.from("chats").update({ status: "completed" }).eq("request_id", requestId);
 
-    if (error) {
+    if (error || error2) {
+        if (error2) {
+            return {
+                success: false,
+                error: error2,
+            }
+        }
+
+        if (error) {
+            return {
+                success: false,
+                error,
+            }
+        }
+        
         return {
             success: false,
             error,
